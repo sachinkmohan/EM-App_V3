a9d390a781f2dbb1e14f4b4c1c4151664c235d9f
Sachin Mohan <sachinkm308@gmail.com>
2022-05-04 23:08:42 +0200

    Added Quiz initial screens

---
+++ screens/quiz/quizScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+    View,
+    Text
+} from 'react-native'
+
+const quizScreen = ({ navigation }) => {
+    return (
+        <View>
+            <Text>Malayalam Quizzes</Text>
+        </View>
+    );
+};
+
+export default quizScreen;
